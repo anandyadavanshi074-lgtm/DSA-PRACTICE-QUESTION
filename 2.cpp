5093d636db3dc7ec8098aa847446f1ad1684a54a
anandyadavanshi <anandyadavanshi074@gmail.com>
2026-06-13 11:38:55 +0530

    Implement player class with data display methods
    Added a player class with member variables and methods to display player data.

--- 2.cpp
+++ 2.cpp
@@ -19,3 +19,54 @@ cout<<sum;
 
 
 }
+
+
+###class####
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class player{
+   private:
+    
+    string name;
+     string age;
+    
+    
+    public:
+    
+    string team;
+    int Age;
+    
+    void data(){
+        cout<<name;
+        cout<<age<<"age";
+    }
+    void data2(){
+        cout<<Age;
+        cout<<team<<" "<<"team";
+        cout<<"empaty";
+    }
+    
+    
+    
+    
+    
+    
+};
+
+int main() {
+    player p1;
+     player p2;
+     player p3;
+    // p1.age="anand";
+    // p1.data();
+    p1.team="rcb";
+    p1.Age=19;
+    p1.data2();
+    p2.team="csk";
+    p2.Age=20;
+    p2.data2();
+    p3.Age=21;
+    p3.data2();
